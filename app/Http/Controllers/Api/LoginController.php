80f9458555ee19c897086cd866b73a2982dc532c
ATC_PhamChinh <pham.chinh@alliedtechcamp.com>
2018-12-19 10:20:35 +0700

    add

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use JWTAuthException;
 use JWTAuth;
 use Validator;
+use App\Common\{AppCommon,ImageCommon,DateCommon};
 
 class LoginController extends ControllerApi
 {
@@ -40,11 +41,30 @@ class LoginController extends ControllerApi
             if(isset($isLogin->customer) && count($isLogin->customers) > 0){
                 $customerId = $isLogin->customers[0]->id;
             }
+            $customer = $this->customerService->find($customerId);
+            if(!isset($customer)){
+                return $this->jsonError([],'Customer not exit.');
+            }
+            $customerInfo = new \StdClass();
+            $customerInfo->customer_id  = $customer->id;
+            $customerInfo->first_name = $customer->first_name;
+            $customerInfo->last_name = $customer->last_name;
+            $customerInfo->email = $customer->email;
+            $customerInfo->mobile_phone = $customer->mobile_phone;
+            $customerInfo->address = AppCommon::nullToEmpty($customer->address);
+            $customerInfo->province_id = AppCommon::nullToEmpty($customer->province_id);
+            $customerInfo->province_name =  isset($customer->province) ? $customer->province->label : '';;
+            $customerInfo->district_id = AppCommon::nullToEmpty($customer->district_id);
+            $customerInfo->district_name =  isset($customer->district) ? $customer->district->label : '';;
+            $customerInfo->gender = AppCommon::nullToEmpty($customer->gender);
+            $customerInfo->birthday = DateCommon::dateFormat($customer->birthday,'d-m-Y');
+            $customerInfo->image_profile = (isset($customer->user) && isset($customer->user->profile_image)) ? ImageCommon::showImage($customer->user->profile_image) : asset('images/no_image_available.jpg');
             return response()->json([
                 'status'=> 0,
                 'token'=> $token,
+                'message'=>'Login success! ',
                 'customer_id' => $customerId,
-                'message'=>'Login success! '
+                'customer' => $customerInfo
             ]);
         }
         return response()->json([
